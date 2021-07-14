9abc438699a7bf8fd83defce19b290c20d9b90b4
Crowdbotics <team@crowdbotics.com>
2021-07-14 18:09:12 +0000

    Initial commit.

---
+++ backend/home/migrations/0001_load_initial_data.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def create_site(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    custom_domain = "dan210714-1-dev-22757.botics.co"
+
+    site_params = {
+        "name": "dan210714_1",
+    }
+    if custom_domain:
+        site_params["domain"] = custom_domain
+
+    Site.objects.update_or_create(defaults=site_params, id=1)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_site),
+    ]
